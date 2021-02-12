e17caf211426dc09d24f09632d01f77fb60566eb
Ahsan <mohd.ahsan.mirza@gmail.com>
2021-02-11 22:14:52 -0500

    Create generate.py

---
+++ generate.py
@@ -0,0 +1,16 @@
+#!/usr/bin/python
+import sys
+import csv
+
+if len(sys.argv) == 2:
+    if sys.argv[1].endswith('.csv'):
+        filename = "analytics_customer.csv"
+        data = list()
+        with  open(filename, 'r', encoding='utf-8-sig') as data:  
+            reader = csv.DictReader(data)
+            data = list(reader)
+        print(data)
+    else:
+        print("Pass in a file that has a csv extension")
+else:
+    print("Pass in the csv filename with the absolute path")
